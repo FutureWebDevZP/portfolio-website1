8b7f760e1d48457d7709783313a188879995a4e9
Zuri P <108591374+FutureWebDevZP@users.noreply.github.com>
2023-02-23 16:19:47 -0500

    added javascript

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
